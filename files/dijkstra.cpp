b8e0e619d4fa2e7cc47a47aca33af39f149ad991
ibuki2003 <main@fuwa.dev>
2020-11-03 18:00:46 +0900

    improve dijkstra

--- files/dijkstra.cpp
+++ files/dijkstra.cpp
@@ -4,21 +4,21 @@ using len_type=ll;
 struct Edge {int to;len_type cost;};
 vector<vector<Edge>> graph;
 void dijkstra(int start, vector<len_type>& dist){
+    using qp = pair<len_type, int>;
     int n=graph.size();
-    fill(all(dist), inf<len_type>);
+    dist.clear();
     dist.resize(n, inf<len_type>);
-    priority_queue<P,vector<P>,greater<P>> q;
+    priority_queue<qp, vector<qp>, greater<qp>> q;
     dist[start]=0;
-    q.push(make_pair(0,start));
+    q.push({0, start});
     while(!q.empty()){
-        P p=q.top();
+        qp p=q.top();
         q.pop();
         int v=p.second;
         if(dist[v]<p.first)continue;
         for(const Edge&e:graph[v]){
-            if(dist[e.to]>dist[v]+e.cost){
-                dist[e.to]=dist[v]+e.cost;
-                q.push(make_pair(dist[e.to],e.to));
+            if(chmin(dist[e.to], dist[v]+e.cost)){
+                q.push({dist[e.to], e.to});
             }
         }
     }
